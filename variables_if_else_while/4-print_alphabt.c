46fe9e3a807fb2d464e96b5de24f06b78ff71f58
Federico Paganini <10647@holbertonstudents.com>
2025-02-14 17:21:17 +0000

    Program 4 fixed

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -12,7 +12,7 @@ int main(void)
 
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	{
-		if (alphabet != 'q' && alphabet != 'w')
+		if (alphabet != 'q' && alphabet != 'e')
 		{
 			putchar(alphabet);
 		}
