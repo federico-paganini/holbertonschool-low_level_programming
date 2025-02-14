8e8864b30cd7cc1ab4abe057836fed61ea092f9c
Federico Paganini <10647@holbertonstudents.com>
2025-02-14 17:00:35 +0000

    Program 3 created

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+/**
+ * main - Prints the alphabet in lowercase, and then in uppercase
+ * Return: Returns 0 (Exit without errors)
+ */
+
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+	for (alphabet = 'A'; alphabet <= 'z'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+
+	putchar('\n);
+
+	return (0);
+}
