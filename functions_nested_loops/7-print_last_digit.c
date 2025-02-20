e2d8361ef2e12b494d3c33fad4a2d304c34cfab3
Federico Paganini <10647@holbertonstudents.com>
2025-02-20 14:13:06 +0000

    [FIX] Program 7 fixed

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -13,6 +13,6 @@ int print_last_digit(int n)
 	int lastDigit =  (n % 10);
 	int abs = ((lastDigit < 0) ? -n : n);
 
-	_putchar('0' + lastDigit);
-	return (lastDigit);
+	_putchar('0' + abs);
+	return (abs);
 }
