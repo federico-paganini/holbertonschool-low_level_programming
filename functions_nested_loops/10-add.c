01bdc70a7fda72904cebf7ec02b65255e762c8cf
Federico Paganini <10647@holbertonstudents.com>
2025-02-20 15:02:42 +0000

    [FEAT] Function 10 created

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - Adds two int and returns the result.
+ *
+ * @a: Frist value to add.
+ * @b: Second value to add.
+ *
+ * Return: Returns the addition result.
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
