289cc270cf63204a6ec39957058445081e2ad3dc
Federico Paganini <10647@holbertonstudents.com>
2025-02-21 17:09:52 +0000

    [FIX] Function 100 fixed

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -10,24 +10,18 @@ void print_times_table(int n)
 {
 	int i;
 	int j;
-
-	for (i = 0; i <= n; i++)
+	
+	if (n > 0 && n < 15)
 	{
-		for (j = 0; j <= n; j++)
+		for (i = 0; i <= n; i++)
 		{
-			int result = (i * j);
+			for (j = 0; j <= n; j++)
+			{
+				int result = (i * j);
 
-			if (result >= 1000)
-			{
-				_putchar('0' + (result / 1000));
-				_putchar('0' + ((result / 100) % 10));
-				_putchar('0' + ((result / 10) % 10));
-				_putchar('0' + (result % 10));
-			}
-			else
-			{
 				if (result >= 100)
-				{	_putchar(' ');
+				{	
+					_putchar(' ');
 					_putchar('0' + (result / 100));
 					_putchar('0' + ((result / 10) % 10));
 					_putchar('0' + (result % 10));
@@ -56,16 +50,16 @@ void print_times_table(int n)
 						}
 					}
 				}
-			}
-			if (j == n)
-			{
-				_putchar('\n');
-			}
-			else
-			{
-				_putchar(',');
+
+				if (j == n)
+				{
+					_putchar('\n');
+				}
+				else
+				{
+					_putchar(',');
+				}
 			}
 		}
 	}
-
 }
