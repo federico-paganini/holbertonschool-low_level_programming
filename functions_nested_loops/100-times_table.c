2bd5bea5caaf5f1d8ca4e836fb210a3b4ded9a77
Federico Paganini <10647@holbertonstudents.com>
2025-02-21 17:28:19 +0000

    [BETTY] Styles fix

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -8,58 +8,37 @@
 
 void print_times_table(int n)
 {
-	int i;
-	int j;
-	
-	if (n > 0 && n < 15)
-	{
-		for (i = 0; i <= n; i++)
-		{
-			for (j = 0; j <= n; j++)
-			{
-				int result = (i * j);
+	int i, j, result;
 
-				if (result >= 100)
-				{	
-					_putchar(' ');
-					_putchar('0' + (result / 100));
-					_putchar('0' + ((result / 10) % 10));
+	if (n < 0 || n > 15)
+		return;
+	for (i = 0; i <= n; i++)
+		for (j = 0, result = 0; j <= n; j++, result = i * j)
+		{
+			if (result >= 100)
+			{
+				_putchar(' ');
+				_putchar('0' + (result / 100));
+				_putchar('0' + ((result / 10) % 10));
+				_putchar('0' + (result % 10));
+			}
+			else
+				if (result >= 10)
+				{
+					_putchar(' '), _putchar(' ');
+					_putchar('0' + (result / 10));
 					_putchar('0' + (result % 10));
 				}
 				else
-				{
-					if (result >= 10)
-					{
-						_putchar(' ');
-						_putchar(' ');
-						_putchar('0' + (result / 10));
-						_putchar('0' + (result % 10));
-					}
+					if (j == 0)
+						_putchar('0' + result);
 					else
 					{
-						if (j == 0)
-						{
-							_putchar('0' + result);
-						}
-						else
-						{
-							_putchar(' ');
-							_putchar(' ');
-							_putchar(' ');
-							_putchar('0' + result);
-						}
+						_putchar(' '), _putchar(' ');
+						_putchar(' ');
+						_putchar('0' + result);
 					}
-				}
-
-				if (j == n)
-				{
-					_putchar('\n');
-				}
-				else
-				{
-					_putchar(',');
-				}
-			}
+			_putchar(j == n ? '\n' : ',');
 		}
-	}
+
 }
