3806084fc912802a4b4ae2ae15dafecabb9b99f4
Federico Paganini <10647@holbertonstudents.com>
2025-02-21 15:44:50 +0000

    [FIX] Program 101 fixed

--- functions_nested_loops/101-natural.c
+++ functions_nested_loops/101-natural.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - computes and prints the sum of all the multiples of 3 or 5 
+ * main - computes and prints the sum of all the multiples of 3 or 5
  * below 1024 (excluded), followed by a new line.
- * 
+ *
  * Return: 0 (Exit without errors).
  */
 
@@ -19,5 +19,7 @@ int main(void)
 			sum += i;
 		}
 	}
+
 	printf("%d\n", sum);
+	return (0);
 }
