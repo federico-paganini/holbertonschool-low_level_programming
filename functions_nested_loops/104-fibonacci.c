16603fe4471368404896d9124e1ad992c012e130
Federico Paganini <10647@holbertonstudents.com>
2025-02-23 00:35:45 +0000

    [FEAT] Program 104 finished

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -1,34 +1,77 @@
 #include <stdio.h>
 
 /**
- * main - Prints the first 50 fibonacci numbers.
+ * addnum - Adds numbers almacenated in an array.
+ *
+ * @arr1: First number almacenated in an array.
+ * @arr2: Second number almacenated in an array.
+ * @result: Array to handle the result.
+ * @decimals: Variable to handle an addition bigger than 9.
+ * @j: Iterator to print the result outside the function.
+ */
+
+void addnum(int *arr1, int *arr2, int *result, int *decimals, int *j)
+{
+	int add;
+	*j = 0;
+
+	do {
+		add = arr1[*j] + arr2[*j] + *decimals;
+
+		if (add < 10)
+		{
+			result[*j] = add;
+			*decimals = 0;
+		}
+		else
+		{
+			result[*j] = add % 10;
+			*decimals = 1;
+		}
+
+		(*j)++;
+	} while ((arr1[(*j) + 1] != 0 || arr2[(*j) + 1] != 0)
+			|| add > 0 || *decimals > 0);
+}
+
+/**
+ * main - Prints the first 98 fibonacci numbers.
  *
  * Return: Returns 0 (Exit without errors).
  */
 
 int main(void)
 {
-	int i;
-	unsigned long prev = 1;
-	unsigned long numb = 2;
+	int i, k, j;
+	int decimals = 0;
+	int prev[25] = {1};
+	int numb[25] = {2};
+	int sum[25];
 
-	printf("%ld, %ld, ", prev, numb);
+	printf("%d, %d, ", prev[0], numb[0]);
 
 	for (i = 3; i <= 98; i++)
 	{
-		unsigned long sum = (prev + numb);
+		addnum(prev, numb, sum, &decimals, &j);
+		for (k = j - 2; k >= 0; k--)
+		{
+			printf("%d", sum[k]);
+		}
 
 		if (i != 98)
 		{
-			printf("%lu, ", sum);
+			printf(", ");
 		}
 		else
 		{
-			printf("%lu\n", sum);
+			putchar('\n');
 		}
 
-		prev = numb;
-		numb = sum;
+		for (k = 0; k < j; k++)
+		{
+			prev[k] = numb[k];
+			numb[k] = sum[k];
+		}
 	}
 
 	return (0);
