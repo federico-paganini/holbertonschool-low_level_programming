39eeee03222aa65177a1369875a88aa1002abbc0
Federico Paganini <10647@holbertonstudents.com>
2025-02-24 17:55:19 +0000

    [FEAT] Function 1 created

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - Swaps the value of two integers.
+ *
+ * @a: First int to swap value.
+ * @b: Second int to swap value.
+ */
+
+void swap_int(int *a, int *b)
+{
+	int n = *a;
+	
+	*a = *b;
+	*b = n;
+}
