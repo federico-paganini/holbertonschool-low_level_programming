8a9db0fd0494415f8ad1c0d93737cc731bc2ccf0
Federico Paganini <10647@holbertonstudents.com>
2025-02-24 13:19:31 +0000

    [FEAT[ Function 3 created

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies two integers.
+ *
+ * @a: First int to multiply.
+ * @b: Second int to multiply.
+ *
+ * Return: Returns the result.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
