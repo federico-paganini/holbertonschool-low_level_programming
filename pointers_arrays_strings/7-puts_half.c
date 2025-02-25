e6347b57b14e710bbc2154772e200ec7776e508e
Federico Paganini <10647@holbertonstudents.com>
2025-02-25 16:04:59 +0000

    [FEAT] Function 7 created

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts_half - Prints a half o a string.
+ *
+ * @str: String to print.
+ */
+
+void puts_half(char *str)
+{
+	int n, length = 0;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+
+	for (n = (length / 2); n < length; n++)
+	{
+		_putchar(str[n]);
+	}
+	_putchar('\n');
+}
+
