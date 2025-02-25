0eff2258154279b6a4f87c6dd9ff631ec1faa9f9
Federico Paganini <10647@holbertonstudents.com>
2025-02-25 18:08:59 +0000

    [FEAT] Function 8 created

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - Prints n elements of an array of integers.
+ *
+ * @a: Array to be printed.
+ * @n: Position of the array to start to print.
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i != (n - 1))
+		{
+			printf(", ");
+		}
+		else
+		{
+			printf("\n");
+		}
+	}
+}
