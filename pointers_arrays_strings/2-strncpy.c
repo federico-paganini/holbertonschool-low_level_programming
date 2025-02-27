cfc6b35e3864637c8d0d035427e032dfe0fea114
Federico Paganini <10647@holbertonstudents.com>
2025-02-27 16:13:38 +0000

    [FIX] Program fix

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -19,7 +19,7 @@ char *_strncpy(char *dest, char *src, int n)
 		return (temp);
 	}
 
-	while (n-- > 0 && (*dest != '\0' || *src != '\0'))
+	while (n-- > 0 && *dest != '\0' && *src != '\0')
 	{
 		*dest = *src;
 		src++;
