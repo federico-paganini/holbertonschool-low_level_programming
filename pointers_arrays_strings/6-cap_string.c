4bc0b7e5aa4f3070b4a4ec9c9b477c48c2a6b7c9
Federico Paganini <10647@holbertonstudents.com>
2025-02-28 17:14:15 +0000

    [FIX] Function

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -15,14 +15,15 @@ char *cap_string(char *str)
 
 	while (str[i] != '\0')
 	{
+		if (str[0] >= 'a' && str[0] <= 'z')
+			str[i + 1] = (((str[i + 1] - '0') - 32) + '0');
+
 		for (j = 0; charset[j] != '\0'; j++)
 		{
 			if (str[i] == charset[j])
 			{
-				if (str[i + 1] >= 'a' && str[i] <= 'z')
-				{
+				if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
 					str[i + 1] = (((str[i + 1] - '0') - 32) + '0');
-				}
 			}
 		}
 		i++;
