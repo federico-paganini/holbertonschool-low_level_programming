2f8a3027de2c53e52e465afa655b54205cb43b14
Federico Paganini <10647@holbertonstudents.com>
2025-03-02 19:41:43 +0000

    [BETTY] Style fix

--- pointers_arrays_strings/103-infinite_add.c
+++ pointers_arrays_strings/103-infinite_add.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * infinite_add - Adds two numbers.
+ * _strlen - Gives the size of a string.
  *
  * @ptrStr: String to get the size.
  *
@@ -26,6 +26,8 @@ int _strlen(char *ptrStr)
  * @n2: Second number to add.
  * @r: Buffer to store the result.
  * @size_r: Buffer size.
+ *
+ * Return: Pointer to the first number possition.
  */
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
@@ -33,21 +35,24 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	int sizen1 = _strlen(n1);
 	int sizen2 = _strlen(n2);
 	int tens = 0, sum;
-	int i = sizen1 - 1, j = sizen2 - 1, k = size_r -1;
-	
+	int i = sizen1 - 1, j = sizen2 - 1, k = size_r - 1;
+
 	r[k] = '\0';
 	k--;
 
-	 if (size_r <= (sizen1 > sizen2 ? sizen1 : sizen2) + 1)
-		         return (0);
+	if (size_r <= ((sizen1 > sizen2) ? sizen1 : sizen2) + 1)
+		return (0);
 
 	while (i >= 0 || j >= 0 || tens)
 	{
-		if (k < 0) return (0);
+		if (k < 0)
+			return (0);
 
 		sum = tens;
-		if (i >= 0) sum += n1[i--] - '0';
-		if (j >= 0) sum += n2[j--] - '0';
+		if (i >= 0)
+			sum += n1[i--] - '0';
+		if (j >= 0)
+			sum += n2[j--] - '0';
 
 		tens = sum / 10;
 		r[k--] = (sum % 10) + '0';
