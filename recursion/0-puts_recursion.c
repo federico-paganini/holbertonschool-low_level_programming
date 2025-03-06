0e84f9d0300557d8aa496e6e288f9d6109472496
Federico Paganini <10647@holbertonstudents.com>
2025-03-06 14:24:33 +0000

    [FIX] Null

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -2,18 +2,21 @@
 
 /**
  * _puts_recursion - Prints a string.
- * 
+ *
  * @s: Char to print.
  */
 
 void _puts_recursion(char *s)
 {
-    if (*s == '\0')
-        return;
-    
-    _putchar(*s);
-    _puts_recursion(s + 1);
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
 
-    if (*(s + 1) == '\0')
-        _putchar('\n');
+	_putchar(*s);
+	_puts_recursion(s + 1);
+
+	if (*(s + 1) == '\0')
+		_putchar('\n');
 }
