9942e4d2ffb9b43188500914186c8ffbeb0a2626
Federico Paganini <10647@holbertonstudents.com>
2025-03-06 14:21:55 +0000

    [FIX] wrong output

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -8,12 +8,12 @@
 
 void _puts_recursion(char *s)
 {
-	_putchar(*s);
-	s++;
+    if (*s == '\0')
+        return;
+    
+    _putchar(*s);
+    _puts_recursion(s + 1);
 
-	if (*s != '\0')
-		_puts_recursion(s);
-
-	if (*(s + 1) == '\0')
-		_putchar('\n');	
+    if (*(s + 1) == '\0')
+        _putchar('\n');
 }
