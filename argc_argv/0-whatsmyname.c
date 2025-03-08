e11cbb9b6655761fad66013f10f9333773cd8ce4
Federico Paganini <10647@holbertonstudents.com>
2025-03-08 23:58:03 +0000

    [FEAT] whatsmyname

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the program.
+ *
+ * @argv: Number of arguments passed to the function.
+ * @argc: Arguments passed to the function.
+ * 
+ * Retrun: Returns 0 (Exit without errors).
+ */
+
+int main(int argv, char **argc)
+{
+	(void)argv;
+	printf("%s\n", argc[0]);
+
+	return (0);
+}
---
+++ argc_argv/README.md
@@ -0,0 +1,3 @@
+Programs and functions description.
+
+0-whatsmyname.c > Program that prints its name.
