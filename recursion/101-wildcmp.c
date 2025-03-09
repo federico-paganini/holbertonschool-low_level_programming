33b792cabc713b12fc2b7c0dd5289cdc53544329
Federico Paganini <10647@holbertonstudents.com>
2025-03-09 17:28:04 +0000

    [FIX] wildcmp

--- recursion/101-wildcmp.c
+++ recursion/101-wildcmp.c
@@ -1,55 +1,24 @@
 #include "main.h"
 
 /**
- * str_length - Function to get the length of the string.
+ * wildcmp - Compares two strings in a wild way.
  *
- * @s: String to get the length.
+ * @s1: First string to compare.
+ * @s2: Second string to compare.
  *
- * Return: The length of the string.
+ * Return: 1 if the strings are identical; 0 in other cases.
  */
-
-int str_length(char *s)
-{
-	return ((*s == '\0') ? 0 : 1 + str_length(s + 1));
-}
-
-char wildstr_letter(char *s, int j)
-{
-	if (j < 0)
-	{
-		return ('*');
-	}
-	else
-	{
-	if  (s[j] >= '!' && s[j] <= '~' && s[j] != '*')
-		return (s[j]);
-	}
-
-	return (wildstr_letter(s, j - 1));
-}
-
-int wildstr_compare(char *s1, char *s2, int i, int j)
-{
-	if (i > 0 && j > 0 && (s1[i] == s2[j] || s2[j] == '*'))
-	{
-			if ((s1[i] == s2[j] || s2[j] == '*') &&
-						(s1[i - 1] == wildstr_letter(s2, j) || wildstr_letter(s2, j) == '*'))
-				return (wildstr_compare(s1, s2, i - 1, j - 1));
-	}
-	else
-	{
-		return (0);
-	}
-
-	return (1);
-}
-
-
 
 int wildcmp(char *s1, char *s2)
 {
-	if (s1[0] == '\0' && (s2[0] == '\0' || s2[0] == '*') && wildstr_letter(s2, str_length(s2)) == '*')
+	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
 
-	return(wildstr_compare(s1, s2, str_length(s1) - 1, str_length(s2) - 1));
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+
+	if (*s2 == '*')
+		return (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)));
+
+	return (0);
 }
