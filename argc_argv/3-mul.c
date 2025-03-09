f1c2304f5b689d66934b7253448c9f85ce7fa115
Federico Paganini <10647@holbertonstudents.com>
2025-03-09 01:00:26 +0000

    [FIX] Segmentation fault

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -12,15 +12,19 @@
 
 int main(int argc, char **argv)
 {
-	int result;
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[2]);
+	int result, num1, num2;
 
 	if (argc < 3)
+	{
 		printf("Error\n");
-
-	result = num1 * num2;
-	printf("%d\n", result);
+	}
+	else
+	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		result = num1 * num2;
+		printf("%d\n", result);
+	}
 
 	return (0);
 }
