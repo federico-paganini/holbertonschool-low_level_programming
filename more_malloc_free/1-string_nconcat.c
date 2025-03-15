ff251b29f285e3a7eb72d4356cf03f025205d510
Federico Paganini <10647@holbertonstudents.com>
2025-03-15 23:08:31 +0000

    [FEAT] string_nconcat

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,75 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * string_nconcat - Concatenates two strings.
+ *
+ * @s1: First string to concat.
+ * @s2: Second string to concat.
+ * @n: Bytes to copy from s2.
+ *
+ * Return: If fails return NULL. Othercase a pointer to the new memory space.
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *concat;
+	unsigned int length1, length2, i, j = 0;
+
+	if (s1 == NULL)
+		return (NULL);
+	if (s2 == NULL)
+	{
+		length2 = 0;
+	}
+	else
+	{
+		length2 = _str_length(s2);
+	}
+
+	length1 = _str_length(s1);
+
+	concat = malloc(sizeof(char) * (length1 + length2 + 2));
+
+	if (concat == NULL)
+		return (NULL);
+
+	for (i = 0; i < length1; i++)
+	{
+		concat[j] = s1[i];
+		j++;
+	}
+
+	concat[j] = ' ';
+	j++;
+
+	for (i = 0; i <= n; i++)
+	{
+		concat[j] = s2[i];
+		j++;
+	}
+
+	concat[j] = '\0';
+
+	return (concat);
+}
+
+
+/**
+ * _str_length - Gets the length of a string.
+ *
+ * @str: String to get the length.
+ *
+ * Return: Length of the string.
+ */
+
+int _str_length(char *str)
+{
+	int length = 0;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+	return (length);
+}
--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -1,3 +1,5 @@
 /* Functions prototypes */
 
+int _str_length(char *);
 void *malloc_checked(unsigned int);
+char *string_nconcat(char *, char *, unsigned int);
