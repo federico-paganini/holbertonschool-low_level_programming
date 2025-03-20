69969c789fb444307457cdb9e6017f9f68bff5b2
Federico Paganini <10647@holbertonstudents.com>
2025-03-20 16:54:53 +0000

    [BETTY] Style fix

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -15,7 +15,7 @@
 int main(int argc, char **argv)
 {
 	int result;
-	
+
 	if (argc != 4)
 	{
 		printf("Error\n");
@@ -27,16 +27,16 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		exit(99);
 	}
-	
+
 	if ((*argv[2] == '/' || *argv[2] == '%') && atoi(argv[3]) == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
-		
+
 	result = get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3]));
-	
+
 	printf("%d\n", result);
-	
+
 	return (0);
 }
