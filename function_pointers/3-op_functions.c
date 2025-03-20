b725cc18caa7fd6f67ef06f5161b737f25830b5d
Federico Paganini <10647@holbertonstudents.com>
2025-03-20 16:47:01 +0000

    [FEAT] Calculator

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,71 @@
+#include "3-calc.h"
+
+/**
+ * op_add - Adds two numbers.
+ *
+ * @a: Frist int to operate.
+ * @b: Second int to operate.
+ *
+ * Return: The result of the operation.
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Substract operation.
+ *
+ * @a: Frist int to operate.
+ * @b: Second int to operate.
+ *
+ * Return: The result of the operation.
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Multiplicates two numbers.
+ *
+ * @a: Frist int to operate.
+ * @b: Second int to operate.
+ *
+ * Return: The result of the operation.
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Divide two numbers.
+ *
+ * @a: Frist int to operate.
+ * @b: Second int to operate.
+ *
+ * Return: The result of the operation.
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Returns the reminder of divide a by b.
+ *
+ * @a: Frist int to operate.
+ * @b: Second int to operate.
+ *
+ * Return: The result of the operation.
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
