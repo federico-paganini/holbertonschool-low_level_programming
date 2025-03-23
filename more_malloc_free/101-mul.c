9b246396bb627865c4aeddad058f6c52072b1b58
Federico Paganini <10647@holbertonstudents.com>
2025-03-23 04:25:57 +0000

    [FIX] surrender

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -3,108 +3,119 @@
 
 /**
  * is_digit - Checks if a string consists only of digits.
- * @s: The string to check.
+ *
+ * @str: The string to check.
  *
  * Return: 1 if all characters are digits, 0 otherwise.
  */
-int is_digit(char *s)
+
+int is_digit(char *str)
 {
-	while (*s)
+	while (*str)
 	{
-		if (*s < '0' || *s > '9')
+		if (*str < '0' || *str > '9')
 			return (0);
-		s++;
+		str++;
 	}
 	return (1);
 }
 
 /**
- * str_length - Returns the length of a string.
- * @s: The input string.
+ * _strlen - Returns the length of a string.
+ *
+ * @str: The input string.
  *
  * Return: Length of the string.
  */
-int str_length(char *s)
-{
-	int len = 0;
 
-	while (s[len])
-		len++;
-	return (len);
+int _strlen(char *str)
+{
+	int length = 0;
+
+	while (str[length])
+		length++;
+	return (length);
 }
 
 /**
  * multiply - Multiplies two positive numbers represented as strings.
+ *
  * @num1: First number.
  * @num2: Second number.
  *
  * Return: Pointer to result string (must be freed).
  */
+
 char *multiply(char *num1, char *num2)
 {
-	int len1 = str_length(num1), len2 = str_length(num2);
-	int result[2000];
+	int len1 = _strlen(num1);
+	int len2 = _strlen(num2);
+	int len_result = len1 + len2;
+	int *result = malloc(len_result * sizeof(int));
 	char *res_str;
-	int i, j, carry, pos, prod, start = 0;
+	int i, j, carry, prod, start = 0;
 
-	for (i = 0; i < 2000; i++)
+	if (!result)
+		return (NULL);
+
+	for (i = 0; i < len_result; i++)
 		result[i] = 0;
+
 	for (i = len1 - 1; i >= 0; i--)
 	{
 		carry = 0;
 		for (j = len2 - 1; j >= 0; j--)
 		{
-			pos = 1999 - ((len1 - 1 - i) + (len2 - 1 - j));
-			prod = (num1[i] - '0') * (num2[j] - '0') + result[pos] + carry;
-			result[pos] = prod % 10;
+			prod = (num1[i] - '0') * (num2[j] - '0') + result[i + j + 1] + carry;
+			result[i + j + 1] = prod % 10;
 			carry = prod / 10;
 		}
-		result[pos - 1] += carry;
+		result[i + j + 1] += carry;
 	}
-	while (start < 2000 && result[start] == 0)
-		start++; 
-	res_str = malloc((2000 - start + 1) * sizeof(char));
+
+	while (start < len_result - 1 && result[start] == 0)
+		start++;
+
+	res_str = malloc(len_result - start + 1);
+
 	if (!res_str)
 	{
-		printf("Error\n");
+		free(result);
 		return (NULL);
 	}
-	for (i = start, j = 0; i < 2000; i++, j++)
+	for (i = start, j = 0; i < len_result; i++, j++)
 		res_str[j] = result[i] + '0';
 	res_str[j] = '\0';
-
+	free(result);
 	return (res_str);
 }
 
 /**
  * main - Entry point, multiplies two positive numbers.
+ *
  * @argc: Number of arguments.
  * @argv: Argument vector.
  *
  * Return: 0 on success, 98 on error.
  */
+
 int main(int argc, char *argv[])
 {
 	char *result;
 
-	/* Validate arguments */
-	if (argc != 3 || !is_digit(argv[1]) || !is_digit(argv[2]))
+	if (argc != 3 || is_digit(argv[1]) == 0 || is_digit(argv[2]) == 0)
 	{
 		printf("Error\n");
 		return (98);
 	}
 
-	/* Perform multiplication */
 	result = multiply(argv[1], argv[2]);
 	if (!result)
+	{
+		printf("Error\n");
 		return (98);
-
-	/* Print result */
+	}
 	printf("%s\n", result);
-
-	/* Free allocated memory */
 	free(result);
-
 	return (0);
 }
-
