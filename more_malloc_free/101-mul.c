4581c9988429d1bca9deb840a1f86bd47fa6ec15
Federico Paganini <10647@holbertonstudents.com>
2025-03-23 04:13:33 +0000

    [FIX] memory leaks

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -45,32 +45,25 @@ char *multiply(char *num1, char *num2)
 	int len1 = str_length(num1), len2 = str_length(num2);
 	int result[2000];
 	char *res_str;
-	int i, j, carry, pos1, pos2, prod, start = 0;
+	int i, j, carry, pos, prod, start = 0;
 
 	for (i = 0; i < 2000; i++)
-		result[i] = -1;
+		result[i] = 0;
 	for (i = len1 - 1; i >= 0; i--)
 	{
 		carry = 0;
 		for (j = len2 - 1; j >= 0; j--)
 		{
-			pos1 = i + j; 
-			pos2 = i + j + 1;
-			if (result[pos2] == -1)
-				result[pos2] = 0;
-			
-			prod = (num1[i] - '0') * (num2[j] - '0') + result[pos2] + carry;
-			result[pos2] = prod % 10;
+			pos = 1999 - ((len1 - 1 - i) + (len2 - 1 - j));
+			prod = (num1[i] - '0') * (num2[j] - '0') + result[pos] + carry;
+			result[pos] = prod % 10;
 			carry = prod / 10;
 		}
-		
-		if (result[i + j + 1] == -1)
-			result[i + j + 1] = 0;
-		result[i + j + 1] += carry;
+		result[pos - 1] += carry;
 	}
-	while (start < 2000 - 1 && result[start] == -1)
-		start++;
-	res_str = malloc(2001 - start);
+	while (start < 2000 && result[start] == 0)
+		start++; 
+	res_str = malloc((2000 - start + 1) * sizeof(char));
 	if (!res_str)
 	{
 		printf("Error\n");
