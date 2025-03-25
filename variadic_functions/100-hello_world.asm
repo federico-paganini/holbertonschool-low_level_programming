91b3847eb9d0f48c0c7a5ef754245cbb59a4eb22
Federico Paganini <10647@holbertonstudents.com>
2025-03-25 13:36:48 +0000

    [FIX] main

--- variadic_functions/100-hello_world.asm
+++ variadic_functions/100-hello_world.asm
@@ -1,11 +1,11 @@
+global main
+
 section .data
     msg db "Hello, World", 10
     len equ $ - msg
 
 section .text
-    global _start
-
-_start:
+main:
     mov rax, 1
     mov rdi, 1
     mov rsi, msg
