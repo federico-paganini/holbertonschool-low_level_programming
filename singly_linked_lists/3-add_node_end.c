c2f7c22e6f4b9f5046d0c3deaca6a51e84c18ed9
Federico Paganini <10647@holbertonstudents.com>
2025-04-06 23:23:37 +0000

    [FEAT] 3-add_node_end

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -4,34 +4,46 @@
 #include <string.h>
 
 /**
- * add_node - Adds a node to the start of a list.
+ * add_node_end - Adds a node at the end of the list.
  *
- * @head: Pointer to the first list item.
- * @str: String to add to the list.
+ * @head: List element passed to function.
+ * @str: String to save in the new element.
  *
- * Return: The adress to the new element or NULL.
+ * Return: The addres of the new element. NULL if fails.
  */
 
-list_t *add_node(list_t **head, const char *str)
+list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *newNode = malloc(sizeof(list_t));
+	list_t *temp;
 
 	if (!newNode)
 		return (NULL);
 
 	newNode->str = strdup(str);
 
-	if (!(newNode->str))
+	if (!newNode->str)
 	{
 		free(newNode);
 		return (NULL);
 	}
 
 	newNode->len = _strlen(newNode->str);
-	newNode->next = *head;
-	*head = newNode;
+	newNode->next = NULL;
 
-	return (*head);
+	if (*head == NULL)
+	{
+		*head = newNode;
+	}
+	else
+	{
+		temp = *head;
+		while (temp->next)
+			temp = temp->next;
+		temp->next = newNode;
+	}
+
+	return (newNode);
 }
 
 /**
@@ -45,9 +57,9 @@ list_t *add_node(list_t **head, const char *str)
 int _strlen(char *str)
 {
 	int length = 0;
-	
+
 	while (str[length] != '\0')
 		length++;
-	
+
 	return (length);
 }
