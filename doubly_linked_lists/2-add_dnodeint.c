6534a9587109b5df44d1e021de8dc1e20771bf6c
Federico Paganini <10647@holbertonstudents.com>
2025-04-07 00:41:31 +0000

    [FIX] base case

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -20,9 +20,18 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 
 	newNode->n = n;
-	newNode->next = *head;
 	newNode->prev = NULL;
-	*head = newNode;
+
+	if (!*head)
+	{
+		newNode->next = NULL;
+		*head = newNode;
+	}
+	else
+	{
+		newNode->next = *head;
+		*head = newNode;
+	}
 
 	return (newNode);
 }
